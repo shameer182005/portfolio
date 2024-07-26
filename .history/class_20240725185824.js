36b968f0166b160ecc57fdae837171fae338d659
shameer182005 <shameerahamedsiraj@gmail.com>
2024-07-26 12:17:01 +0530

    ge

---
+++ .history/class_20240725185824.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!'); // Replace this with actual form submission logic
+});
